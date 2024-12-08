bfbef32560ea7b82511f988f431faa1de2926145
r4ulcl <raulcalvolaorden@gmail.com>
2024-12-08 09:58:44 +0000

    Update createCert script and certificate

--- certs/createCert.sh
+++ certs/createCert.sh
@@ -1,17 +1,22 @@
+#!/bin/bash
+
 #https://wiki.innovaphone.com/index.php?title=Howto:802.1X_EAP-TLS_With_FreeRadius#Creation_Of_A_Self-Signed_CA_Certificate
-# 2048 
 
-#Clean
-rm ca.* client.* server.*
+# Global variable for certificate validity in days (10 years)
+CERT_VALIDITY_DAYS=3650
 
-#Creation Of A Self-Signed CA Certificate
+# Clean up any existing files
+rm -f ca.* client.* server.*
+
+# Creation Of A Self-Signed CA Certificate
 openssl genrsa -out ca.key 2048
 
-echo '[ req ]
+cat <<EOF > ca.conf
+[ req ]
 default_bits       = 2048
 distinguished_name = req_DN
 string_mask        = nombstr
- 
+
 [ req_DN ]
 countryName                     = "1. Country Name             (2 letter code)"
 countryName_default             = ES
@@ -30,13 +35,14 @@ commonName_max                  = 64
 commonName_default              = WiFiChallenge CA
 emailAddress                    = "7. Email Address            (eg, name@FQDN)"
 emailAddress_max                = 40
-emailAddress_default            = ca@WiFiChallenge.com' > ca.conf
+emailAddress_default            = ca@WiFiChallenge.com
+EOF
 
 openssl req -config ca.conf -new -key ca.key -out ca.csr
 
-echo '
+cat <<EOF > ca.ext
 extensions = x509v3
- 
+
 [ x509v3 ]
 basicConstraints      = CA:true,pathlen:0
 crlDistributionPoints = URI:http://WiFiChallenge.com/ca/mustermann.crl
@@ -44,25 +50,25 @@ nsCertType            = sslCA,emailCA,objCA
 nsCaPolicyUrl         = "http://WiFiChallenge.com/ca/policy.htm"
 nsCaRevocationUrl     = "http://WiFiChallenge.com/ca/heimpold.crl"
 nsComment             = "WiFiChallenge CA"
-' > ca.ext
+EOF
 
-openssl x509 -days 1095 -extfile ca.ext -signkey ca.key -in ca.csr -req -out ca.crt
+openssl x509 -days $CERT_VALIDITY_DAYS -extfile ca.ext -signkey ca.key -in ca.csr -req -out ca.crt
 
-#Creation Of A Server Certificate
+# Creation Of A Server Certificate
 openssl genrsa -out server.key 2048
-echo '
+
+cat <<EOF > server.conf
 [ req ]
 default_bits       = 2048
 distinguished_name = req_DN
 string_mask        = nombstr
- 
+
 [ req_DN ]
 countryName                     = "1. Country Name             (2 letter code)"
 countryName_default             = ES
 countryName_min                 = 2
 countryName_max                 = 2
 stateOrProvinceName             = "2. State or Province Name   (full name)    "
-#stateOrProvinceName_default     = 
 localityName                    = "3. Locality Name            (eg, city)     "
 localityName_default            = Madrid
 0.organizationName              = "4. Organization Name        (eg, company)  "
@@ -75,30 +81,32 @@ commonName_default              = WiFiChallenge CA
 emailAddress                    = "7. Email Address            (eg, name@FQDN)"
 emailAddress_max                = 40
 emailAddress_default            = server@WiFiChallenge.com
+EOF
 
-' > server.conf 
+cat <<EOF > server.ext
+extensions = x509v3
 
-echo 'extensions = x509v3
- 
 [ x509v3 ]
 nsCertType       = server
 keyUsage         = digitalSignature,nonRepudiation,keyEncipherment
-extendedKeyUsage = msSGC,nsSGC,serverAuth' > server.ext
+extendedKeyUsage = msSGC,nsSGC,serverAuth
+EOF
 
 echo -ne '01' > ca.serial
 
 openssl req -config server.conf -new -key server.key -out server.csr
 
-openssl x509 -days 730 -extfile server.ext -CA ca.crt -CAkey ca.key -CAserial ca.serial -in server.csr -req -out server.crt
+openssl x509 -days $CERT_VALIDITY_DAYS -extfile server.ext -CA ca.crt -CAkey ca.key -CAserial ca.serial -in server.csr -req -out server.crt
 
-#Creation Of A Client Certificate
+# Creation Of A Client Certificate
 openssl genrsa -out client.key 2048
 
-echo '[ req ]
+cat <<EOF > client.conf
+[ req ]
 default_bits       = 2048
 distinguished_name = req_DN
 string_mask        = nombstr
- 
+
 [ req_DN ]
 countryName                     = "1. Country Name             (2 letter code)"
 countryName_default             = ES
@@ -111,20 +119,24 @@ localityName_default            = Madrid
 0.organizationName              = "4. Organization Name        (eg, company)  "
 0.organizationName_default      = WiFiChallenge
 organizationalUnitName          = "5. Organizational Unit Name (eg, section)  "
-#organizationalUnitName_default  = 
 commonName                      = "6. Common Name              (eg, CA name)  "
 commonName_max                  = 64
 commonName_default              = WiFiChallenge CA
 emailAddress                    = "7. Email Address            (eg, name@FQDN)"
 emailAddress_max                = 40
-emailAddress_default            = client@WiFiChallenge.com' > client.conf
+emailAddress_default            = client@WiFiChallenge.com
+EOF
 
-echo 'extensions = x509v3
- 
+cat <<EOF > client.ext
+extensions = x509v3
+
 [ x509v3 ]
 nsCertType = client,email,objsign
-keyUsage   = digitalSignature,nonRepudiation,keyEncipherment,dataEncipherment' > client.ext
+keyUsage   = digitalSignature,nonRepudiation,keyEncipherment,dataEncipherment
+EOF
 
 openssl req -config client.conf -new -key client.key -out client.csr
-openssl x509 -days 730 -extfile client.ext -CA ca.crt -CAkey ca.key -CAserial ca.serial -in client.csr -req -out client.crt
+
+openssl x509 -days $CERT_VALIDITY_DAYS -extfile client.ext -CA ca.crt -CAkey ca.key -CAserial ca.serial -in client.csr -req -out client.crt
+
 cat client.crt client.key > client.pem.crt
