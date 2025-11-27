83f06565aa6d1069ee894ac2125c3a56cbd7fd30
Raúl Calvo Laorden <25440046+r4ulcl@users.noreply.github.com>
2025-11-27 18:05:17 +0100

    clients vars - untested

--- Clients/config/cronClients.sh
+++ Clients/config/cronClients.sh
@@ -3,7 +3,6 @@
 #Load variables
 set -a
 source /root/wlan_config_clients
-PHISHING_PASS='tommy1'
 
 
 function retry { 
@@ -37,63 +36,133 @@ do
     wait $!
 	sleep 60
 done &
-
 # Normal clients curls
 while :
 do
-	# MGT
-	curl -s "http://$MAC_MGT_MSCHAP.1/login.php" --interface $WLAN_MGT_MSCHAP --compressed -H 'Content-Type: application/x-www-form-urlencoded' -H 'Connection: keep-alive' --data-raw 'Username=CONTOSO%5Cjuan.tr&Password=Secret%21&Submit=Login' -c /tmp/userjuan -b /tmp/userjuan &
-	curl -s "http://$MAC_MGT_GTC.1/login.php" --interface $WLAN_MGT_GTC --compressed -H 'Content-Type: application/x-www-form-urlencoded' -H 'Connection: keep-alive' --data-raw 'Username=CONTOSO%5CAdministrator&Password=SuperSecure%40%21%40&Submit=Login' -c /tmp/userAdmin -b /tmp/userAdmin &
+	# MGT MSCHAP
+	curl -s "http://$MAC_MGT_MSCHAP.1/login.php" --interface $WLAN_MGT_MSCHAP --compressed \
+		-H 'Content-Type: application/x-www-form-urlencoded' -H 'Connection: keep-alive' \
+		--data-urlencode "Username=$IDENTITY_MGT_MSCHAP" \
+		--data-urlencode "Password=$PASS_MGT_MSCHAP_CLEAR" \
+		--data-urlencode "Submit=Login" \
+		-c /tmp/userjuan -b /tmp/userjuan &
+
+	# MGT GTC
+	curl -s "http://$MAC_MGT_GTC.1/login.php" --interface $WLAN_MGT_GTC --compressed \
+		-H 'Content-Type: application/x-www-form-urlencoded' -H 'Connection: keep-alive' \
+		--data-urlencode "Username=$IDENTITY_MGT_GTC" \
+		--data-urlencode "Password=$PASS_MGT_GTC_CLEAR" \
+		--data-urlencode "Submit=Login" \
+		-c /tmp/userAdmin -b /tmp/userAdmin &
 
 	# MGT Relay
-	curl -s "http://$IP_MGT_RELAY.1/login.php" --interface $WLAN_MGT_RELAY --compressed -H 'Content-Type: application/x-www-form-urlencoded' -H 'Connection: keep-alive' --data-raw 'Username=CONTOSOREG%5Cluis.da&Password=u89gh68!6fcv56ed&Submit=Login' -c /tmp/userluis -b /tmp/userluis  &
+	curl -s "http://$IP_MGT_RELAY.1/login.php" --interface $WLAN_MGT_RELAY --compressed \
+		-H 'Content-Type: application/x-www-form-urlencoded' -H 'Connection: keep-alive' \
+		--data-urlencode "Username=$IDENTITY_MGT_RELAY" \
+		--data-urlencode "Password=$PASS_MGT_RELAY_CLEAR" \
+		--data-urlencode "Submit=Login" \
+		-c /tmp/userluis -b /tmp/userluis &
 
 	# MGT TLS
-	curl -s "http://$IP_TLS.1/login.php" --interface $WLAN_TLS --compressed -H 'Content-Type: application/x-www-form-urlencoded' -H 'Connection: keep-alive' --data-raw 'Username=GLOBAL%5CGlobalAdmin&Password=SuperSuperSecure%40%21%40&Submit=Login' -c /tmp/userGlobal -b /tmp/userGlobal  &
+	curl -s "http://$IP_TLS.1/login.php" --interface $WLAN_TLS --compressed \
+		-H 'Content-Type: application/x-www-form-urlencoded' -H 'Connection: keep-alive' \
+		--data-urlencode "Username=$IDENTITY_MGT_TLS" \
+		--data-urlencode "Password=$PASS_MGT_TLS_CLEAR" \
+		--data-urlencode "Submit=Login" \
+		-c /tmp/userGlobal -b /tmp/userGlobal &
 
 	# MGT TLS PHISHING
-	# TODO use template, get redirect and POST
-	curl -s "http://$IP_TLS_PHISHING.1/login.php" --interface $WLAN_TLS_PHISHING --compressed -H 'Content-Type: application/x-www-form-urlencoded' -H 'Connection: keep-alive' --data-raw 'Username=GLOBAL%5CGlobalManager&Password=password1%40%21&Submit=Login' -c /tmp/userPhishing -b /tmp/userPhishing  &
+	curl -s "http://$IP_TLS_PHISHING.1/login.php" --interface $WLAN_TLS_PHISHING --compressed \
+		-H 'Content-Type: application/x-www-form-urlencoded' -H 'Connection: keep-alive' \
+		--data-urlencode "Username=$IDENTITY_MGT_PHISHING" \
+		--data-urlencode "Password=$PASS_MGT_PHISHING_CLEAR" \
+		--data-urlencode "Submit=Login" \
+		-c /tmp/userPhishing -b /tmp/userPhishing &
 
-	# PSK, only login if cookies error
+	# WPA PSK (login only if redirect)
 	STATUS=`curl -o /dev/null -w '%{http_code}\n' -s "http://$IP_WPA_PSK.1/lab.php" -c /tmp/userTest1 -b /tmp/userTest1`
 	if [ "$STATUS" -eq 302 ] ; then
-		curl -s "http://$IP_WPA_PSK.1/login.php" --interface $WLAN_WPA_PSK --compressed -H 'Content-Type: application/x-www-form-urlencoded' -H 'Connection: keep-alive' --data-raw 'Username=test1&Password=OYfDcUNQu9PCojb&Submit=Login' -c /tmp/userTest1 -b /tmp/userTest1 &
+		curl -s "http://$IP_WPA_PSK.1/login.php" --interface $WLAN_WPA_PSK --compressed \
+			-H 'Content-Type: application/x-www-form-urlencoded' -H 'Connection: keep-alive' \
+			--data-urlencode "Username=$USER_WEB_WPA_PSK" \
+			--data-urlencode "Password=$PASS_WEB_WPA_PSK_CLEAR" \
+			--data-urlencode "Submit=Login" \
+			-c /tmp/userTest1 -b /tmp/userTest1 &
 	fi
 
 	STATUS=`curl -o /dev/null -w '%{http_code}\n' -s "http://$IP_WPA_PSK2.1/lab.php" -c /tmp/userTest2 -b /tmp/userTest2`
 	if [ "$STATUS" -eq 302 ] ; then
-		curl -s "http://$IP_WPA_PSK2.1/login.php" --interface $WLAN_WPA_PSK2 --compressed -H 'Content-Type: application/x-www-form-urlencoded' -H 'Connection: keep-alive' --data-raw 'Username=test2&Password=2q60joygCBJQuFo&Submit=Login' -c /tmp/userTest2 -b /tmp/userTest2 &
+		curl -s "http://$IP_WPA_PSK2.1/login.php" --interface $WLAN_WPA_PSK2 --compressed \
+			-H 'Content-Type: application/x-www-form-urlencoded' -H 'Connection: keep-alive' \
+			--data-urlencode "Username=$USER_WEB_WPA_PSK2" \
+			--data-urlencode "Password=$PASS_WEB_WPA_PSK2_CLEAR" \
+			--data-urlencode "Submit=Login" \
+			-c /tmp/userTest2 -b /tmp/userTest2 &
 	fi
 
-	# PSK NOAPP
-	curl -s "http://$WLAN_PSK_NOAP.1/login.php" --interface $WLAN_PSK_NOAP --compressed -H 'Content-Type: application/x-www-form-urlencoded' -H 'Connection: keep-alive' --data-raw 'Username=anon1&Password=CRgwj5fZTo1cO6Y&Submit=Login' -c /tmp/userAnon1  -b /tmp/userAnon1 &
-	curl -s "http://$WLAN_PSK_NOAP2.1/login.php" --interface $WLAN_PSK_NOAP2 --compressed -H 'Content-Type: application/x-www-form-urlencoded' -H 'Connection: keep-alive' --data-raw 'Username=anon1&Password=CRgwj5fZTo1cO6Y&Submit=Login' -c /tmp/userAnon11 -b /tmp/userAnon11 &
+	# PSK NOAP
+	curl -s "http://$WLAN_PSK_NOAP.1/login.php" --interface $WLAN_PSK_NOAP --compressed \
+		-H 'Content-Type: application/x-www-form-urlencoded' -H 'Connection: keep-alive' \
+		--data-urlencode "Username=anon1" \
+		--data-urlencode "Password=$PASS_PSK_NOAP_CLEAR" \
+		--data-urlencode "Submit=Login" \
+		-c /tmp/userAnon1 -b /tmp/userAnon1 &
 
-	# OPEN
-	curl -s "http://$IP_OPN1.1/login.php" --interface $WLAN_OPN1 --compressed -H 'Content-Type: application/x-www-form-urlencoded' -H 'Connection: keep-alive' --data-raw 'Username=free1&Password=Jyl1iq8UajZ1fEK&Submit=Login' -c /tmp/userFree1 -b /tmp/userFree1 &
-	curl -s "http://$IP_OPN2.1/login.php" --interface $WLAN_OPN2 --compressed -H 'Content-Type: application/x-www-form-urlencoded' -H 'Connection: keep-alive' --data-raw 'Username=free2&Password=5LqwwccmTg6C39y&Submit=Login' -c /tmp/userFree2 -b /tmp/userFree2 &
-	curl -s "http://$IP_OPN3.1/login.php" --interface $WLAN_OPN3 --compressed -H 'Content-Type: application/x-www-form-urlencoded' -H 'Connection: keep-alive' --data-raw 'Username=free1&Password=Jyl1iq8UajZ1fEK&Submit=Login' -c /tmp/userFree11 -b /tmp/userFree11 &
+	curl -s "http://$WLAN_PSK_NOAP2.1/login.php" --interface $WLAN_PSK_NOAP2 --compressed \
+		-H 'Content-Type: application/x-www-form-urlencoded' -H 'Connection: keep-alive' \
+		--data-urlencode "Username=$USER_WEB_WPA_PSK_NOAP" \
+		--data-urlencode "Password=$PASS_WEB_WPA_PSK_NOAP_CLEAR" \
+		--data-urlencode "Submit=Login" \
+		-c /tmp/userAnon11 -b /tmp/userAnon11 &
+
+	# OPEN (kept literal because you have no variables defined)
+	curl -s "http://$IP_OPN1.1/login.php" --interface $WLAN_OPN1 --compressed \
+		-H 'Content-Type: application/x-www-form-urlencoded' -H 'Connection: keep-alive' \
+		--data-urlencode "Username=$USER_WEB_OPN1" \
+		--data-urlencode "Password=$PASS_WEB_OPN1_CLEAR" \
+		--data-urlencode "Submit=Login" \
+		-c /tmp/userFree1 -b /tmp/userFree1 &
+
+	curl -s "http://$IP_OPN2.1/login.php" --interface $WLAN_OPN2 --compressed \
+		-H 'Content-Type: application/x-www-form-urlencoded' -H 'Connection: keep-alive' \
+		--data-urlencode "Username=$USER_WEB_OPN2" \
+		--data-urlencode "Password=$PASS_WEB_OPN2_CLEAR" \
+		--data-urlencode "Submit=Login" \
+		-c /tmp/userFree2 -b /tmp/userFree2 &
+
+	curl -s "http://$IP_OPN3.1/login.php" --interface $WLAN_OPN3 --compressed \
+		-H 'Content-Type: application/x-www-form-urlencoded' -H 'Connection: keep-alive' \
+		--data-urlencode "Username=$USER_WEB_OPN3" \
+		--data-urlencode "Password=$PASS_WEB_OPN3_CLEAR" \
+		--data-urlencode "Submit=Login" \
+		-c /tmp/userFree3 -b /tmp/userFree3 &
 
 	# WPA3 Downgrade
-    curl -s "http://$IP_DOWNGRADE.1/login.php" --interface $WLAN_DOWNGRADE --compressed -H 'Content-Type: application/x-www-form-urlencoded' -H 'Connection: keep-alive' --data-raw 'Username=manager1&Password=Aaa23dF4r&Submit=Login' -c /tmp/userManager1 -b /tmp/userManager1 &
+	curl -s "http://$IP_DOWNGRADE.1/login.php" --interface $WLAN_DOWNGRADE --compressed \
+		-H 'Content-Type: application/x-www-form-urlencoded' -H 'Connection: keep-alive' \
+		--data-urlencode "Username=$USER_WEB_DOWNGRADE" \
+		--data-urlencode "Password=$PASS_WEB_DOWNGRADE_CLEAR" \
+		--data-urlencode "Submit=Login" \
+		-c /tmp/userManager1 -b /tmp/userManager1 &
 
-    wait $!
+	wait $!
 	sleep 10
 done &
 
 # Phishing 
 while :
 do
-	# TODO Phishing client connect
-	#dhclien-wifichallenge -r $WLAN_TLS_PHISHING 2> /tmp/dhclien-wifichallenge
 	timeout -k 1 5s dhclien-wifichallenge -v $WLAN_TLS_PHISHING 2> /tmp/dhclien-wifichallenge
 	SERVER=`grep -E -o "from (25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)" /tmp/dhclien-wifichallenge | awk '{print $2}' | head -n 1`
+
 	if [ -n "$SERVER" ]; then
 		URL=`curl -L -s -o /dev/null -w %{url_effective} "http://$SERVER/" -c /tmp/userTLSPhishing -b /tmp/userTLSPhishing`
-		curl -L -s "$URL" -H 'Content-Type: application/x-www-form-urlencoded' --data-raw "username=CORPO\god&password=$PHISHING_PASS" -c /tmp/userTLSPhishing -b /tmp/userTLSPhishing > /dev/null
+		curl -L -s "$URL" -H 'Content-Type: application/x-www-form-urlencoded' \
+			--data-urlencode "username=$IDENTITY_MGT_PHISHING_GOD_DOMAIN\\$IDENTITY_MGT_PHISHING_GOD_USER" \
+			--data-urlencode "password=$PHISHING_PASS_CLEAR" \
+			-c /tmp/userTLSPhishing -b /tmp/userTLSPhishing > /dev/null
 	fi
-	# avoid spam
+
 	sleep 1
 done &
 
@@ -118,11 +187,11 @@ do
 			echo flock
 			
 			# run smbmap under cpulimit and timeout (background)
-			timeout -k 1 10s cpulimit -l 30 -f -- /usr/bin/smbmap -d 'CORPO' -u 'god' -p "$PHISHING_PASS" -H "$SERVER" >/dev/null 2>&1 &
+			timeout -k 1 10s cpulimit -l 30 -f -- /usr/bin/smbmap -d "$IDENTITY_MGT_PHISHING_GOD_DOMAIN" -u "$IDENTITY_MGT_PHISHING_GOD_USER" -p "$PHISHING_PASS_CLEAR" -H "$SERVER" >/dev/null 2>&1 &
 
 			sleep 0.5
             # run smbmap under cpulimit and timeout (foreground)
-            timeout -k 1 20s cpulimit -l 30 -f -- /usr/bin/smbmap -d 'CORPO' -u 'god' -p "$PHISHING_PASS" -H "$SERVER" >/dev/null 2>&1
+            timeout -k 1 20s cpulimit -l 30 -f -- /usr/bin/smbmap -d "$IDENTITY_MGT_PHISHING_GOD_DOMAIN" -u "IDENTITY_MGT_PHISHING_GOD_USER" -p "$PHISHING_PASS_CLEAR" -H "$SERVER" >/dev/null 2>&1
         ) 9>/var/lock/smbmap.lock
 
     else
